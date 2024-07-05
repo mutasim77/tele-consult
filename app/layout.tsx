e07a2a5735e414f7a2242552e5baf75cb5c65fc1
 <96326525+mutasim77@users.noreply.github.com>
2024-07-05 16:13:40 +0500

    🛠️ feat(layout): make minor changes and add SEO improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
-import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { WebSocketProvider } from "@/providers/SocketProvider";
-import SocketIndicator from "@/components/SocketIndicator";
+import { Footer, Header } from "@/components";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -12,8 +11,31 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "TeleConsult",
-  description: "Online consultation service for telecom operators",
+  title: "TeleConsult | Real-time Online Consultation for Telecom Operators",
+  description: "Get instant support from telecom experts. TeleConsult offers secure, OTP-authenticated chats with mobile phone operators for quick problem-solving and guidance.",
+  keywords: "telecom consultation, online support, mobile operator chat, OTP authentication, real-time communication",
+  openGraph: {
+    title: "TeleConsult - Connect with Telecom Experts Instantly",
+    description: "Need help with your mobile service? Chat with telecom operators in real-time. Secure, fast, and efficient support at your fingertips.",
+
+    images: [
+      {
+        url: "https://www.mutasim.top/og?title=TeleConsult%20|%20Real-time%20Online%20Consultation%20for%20Telecom%20Operators",
+        width: 1200,
+        height: 630,
+        alt: "TeleConsult App Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: "index, follow",
+  authors: {
+    name: "Mutasim",
+    url: "https://www.mutasim.top/"
+  },
+  viewport: "width=device-width, initial-scale=1",
+  themeColor: "#fbbf24",
 };
 
 export default function RootLayout({
@@ -23,16 +45,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${poppins.className} bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text`}>
+      <body className={`${poppins.className} bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text  w-full h-full  min-h-screen flex flex-col justify-center items-center max-w-3xl m-auto px-4 xs:px-10`}>
         <ThemeProvider>
           <WebSocketProvider>
-            <div className="flex items-center justify-center min-h-screen">
-              <div className="relative">
-                <ThemeSwitcher className="absolute top-4 right-4 z-50" />
-                <SocketIndicator />
+            <Header />
+            <main className="h-full w-full justify-center items-center m-auto py-4 xs:py-10">
+              <div className="bg-light-white dark:bg-dark-grayDarkest shadow-lg p-6 max-w-[900px] mx-auto border border-white/10 rounded-3xl">
                 {children}
               </div>
-            </div>
+            </main>
+            <Footer />
           </WebSocketProvider>
         </ThemeProvider>
       </body>
