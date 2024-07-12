418e614fbc7a852ce083082fe3d3f1a7cadbbe34
 <96326525+mutasim77@users.noreply.github.com>
2024-07-12 18:31:47 +0500

    ✨ feat: Add global TypeScript interfaces

--- types/next.d.ts
+++ types/next.d.ts
@@ -1,11 +1,43 @@
-import { Server as NetServer, Socket } from 'net';
-import { NextApiResponse } from 'next';
-import { Server as SocketIOServer } from 'socket.io';
+declare global {
+    interface Message {
+        sender_id: string;
+        content: string;
+        is_image: boolean;
+        created_at: string;
+    }
 
-export type NextApiResponseServerIO = NextApiResponse & {
-    socket: Socket & {
-        server: NetServer & {
-            io: SocketIOServer;
+    interface ChatMessagesProps {
+        messages: Message[];
+        currentUserId: string;
+        formatTime: (timestamp: string) => string;
+    }
+
+    interface ChatInputProps {
+        inputMessage: string;
+        setInputMessage: (message: string) => void;
+        sendMessage: (message: string) => void;
+        handleFileUpload: (event: React.ChangeEvent<HTMLInputElement>) => void;
+        isDisabled: boolean;
+        isUploading: boolean;
+    }
+
+    interface Chat {
+        id: string;
+        created_at?: string;
+        status?: string | null;
+        user_id: string;
+        operator_id: string;
+        operators?: {
+            username: string;
+            is_online: boolean;
+        }
+        users?: {
+            id: string;
+            name: string;
+            language: string;
+            is_online: boolean;
         }
     }
 }
+
+export { }
