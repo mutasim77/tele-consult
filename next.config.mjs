4b9c21eff1ce6c1dbcf147238ece0189ce173329
 <96326525+mutasim77@users.noreply.github.com>
2024-07-12 18:32:28 +0500

    ✨ feat: Update next.config.js to allow image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: ['ucbtidlhyfpiknjfrndg.supabase.co'],
+    },
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback = {
